38e4ea986c839d9cae2499e2028fb093a38cb53a
lucablejer <lblejer@udesa.edu.ar>
2025-06-06 09:28:57 -0300

    Cambios en punto 4, 5 y termine el 3. Tambien merge}

---
+++ js/detailgenre.js
@@ -0,0 +1,2 @@
+// Sitio donde el usuario selecciona un genero y lo redirecciona a una pagina con todas las peliculas/series de ese genero // 
+
---
+++ js/selectedgenre.js
@@ -0,0 +1,2 @@
+// Cuando un usuario selecciona el genero dentro de una pelicula/serie, lo redireccione hacia la pagina del genero seleccionado // 
+
