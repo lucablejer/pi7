bb467eccad71ed438748fa738f33eea8bc6a8e4c
ignaciobabich <ifernandezbabich@udesa.edu.ar>
2025-06-06 12:25:14 -0300

    termine movie y serie genre

--- js/seriegenre.js
+++ js/seriegenre.js
@@ -1,1 +1,31 @@
 // Atrapar los selectores //
+
+let queryString = location.search
+let queryObj = new URLSearchParams (queryString)
+let movie =  queryObj.get ('.typem')
+let gen = document.querySelector('.generos') 
+let generos = ""; 
+
+let url = 'https://api.themoviedb.org/3/genre/tv/list?api_key=b04e301645ef571f2efbccb360411716&language=es-ES'
+
+fetch (url)
+.then(function(response) {
+    return response.json()
+  })
+.then(function(data) {
+  
+    for (let i=0; i < data.genres.length; i++){
+        console.log('vuelta ', [i], generos);
+        generos += `<a class=".generos" href= "detailgenre.html?id=${data.genres[i].id}&nombre=${data.genres[i].name}&tipo=movie"> ${data.genres[i].name}</a>`
+    }
+    
+    gen.innerHTML = generos
+
+   })
+   
+
+.catch(function(error) {
+    console.log("Error: " + error);
+ })
+ 
+  
